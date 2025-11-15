d55a1d69051bf87ad6b6143e7628c54d5bbd2b01
msam10 <msamantha1224@gmail.com>
2025-11-14 22:04:04 -0400

    Decoración inicial de carta

--- proyecto_cartas/src/App.tsx
+++ proyecto_cartas/src/App.tsx
@@ -2,19 +2,26 @@
 import Cartadetalle from './Componentes/Carta'
 
 function App() {
-
+  
   return (
-
+    <div className="bg-linear-to-r from-cyan-500 to-blue-500">
     <Cartadetalle 
     ataque={8000}
     nombre="Luffy"
     defensa={6000}
-    descripcion="Luffy,usuario de la fruta del diablo; hito hito no mi, modelo:NIKA"
+    descripcion="Luffy,usuario de la fruta del diablo; hito hito no mi."
     imagen="https://preview.redd.it/what-makes-luffy-such-a-likable-protagonist-v0-lhdd2872qurb1.jpg?width=320&crop=smart&auto=webp&s=214ccf69b93d7c4ce7001b19c9d1bbd12ec98ba2"
     tipo="ataque fisico\ofensivo"
     numero={1}
     
     />
+
+
+
+
+
+    
+    </div>
   )
 }
 
--- proyecto_cartas/src/Componentes/Carta.tsx
+++ proyecto_cartas/src/Componentes/Carta.tsx
@@ -18,18 +18,19 @@ imagen,
 nombre,
 numero,
 tipo,
-}: Props 
+}: Props    
 ) {
  return (
-    <div className="flex flex-col items-center bg-red-700 border-4 roundeg-lg w-90">
+    <div className="flex flex-col items-center bg-red-700 border-4 rounded-lg w-90 font-mono text-orange-100">
     <h3>
         {nombre} (#{numero})
     </h3>
-    <img src={imagen} alt={nombre} />
-     <p>ataque:{ataque}</p>
-      <p>defensa:{defensa}</p>
-      <p>descripcion:{descripcion}</p>
-      <p>tipo:{tipo}</p>
+    <img className="w-80 h-80 border-3 border-dashed rounded-sm border-blue-500" src={imagen} alt={nombre} />
+ 
+     <p className="pl-5 pr-5 " >ataque:{ataque}</p>
+      <p className="pl-5 pr-5" >defensa:{defensa}</p>
+      <p  className="pl-5 pr-5" >descripcion:{descripcion}</p>
+      <p className="pl-5 pr-5">tipo:{tipo}</p>
 
     </div>
  )
